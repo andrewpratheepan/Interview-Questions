900aa21c85e043b99808a1ccf7de28c91ae1e0ec
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-01-08 20:55:26 -0500

    minor changes

--- venv/Chapter 1/1_7.py
+++ venv/Chapter 1/1_7.py
@@ -1,4 +1,18 @@
+import pprint
 def rotateMatrix(a):#a is nXn
-    b = [[]]
+    b = []
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            b[i].append(a[i])
+'''
+def test():
+    b = []
+    for i in range(6):
+        b.append([])
+        for j in range(6):
+            b[i].append(1)
+    for i in range(len(b)):
+        print(b)'''
+
 
     #TODO Q1_7
